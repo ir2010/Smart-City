69cba7d4cf9c2db2ba63bc814e62be03ff987118
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-04-10 18:29:16 +0530

    code cleaned

---
+++ app/src/main/java/com/ir/smartcity/StoreRoom.java
@@ -0,0 +1,43 @@
+package com.ir.smartcity;
+
+public class StoreRoom {
+
+
+//    private void initdata() {
+//
+//        jobList = new ArrayList<>();
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//        jobList.add(new Job("Teach my Std. 5 kid", "Mar 23 22:30", "Shastrinagar, Munger", "The job is to teach my child who is in Std. 5 for his exams. Duration will be 2 weeks. Subjects - English, Hindi, Science", "900"));
+//    }
+//
+//    private void initRecyclerView()
+//    {
+//        jobListView=findViewById(R.id.jobs_list);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        layoutManager.setOrientation(RecyclerView.VERTICAL);
+//        jobListView.setLayoutManager(layoutManager);
+//        adapter=new JobAdapter(jobList);
+//        jobListView.setAdapter(adapter);
+//        adapter.notifyDataSetChanged();
+//    }
+}
