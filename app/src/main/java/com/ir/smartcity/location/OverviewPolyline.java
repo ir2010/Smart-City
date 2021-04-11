2fbb422066e4045a36be9ddc7673481dbbd0a95b
ishuraj2010@gmail.com <ishuraj2010@gmail.com>
2021-04-11 09:15:50 +0530

    restructured

---
+++ app/src/main/java/com/ir/smartcity/location/OverviewPolyline.java
@@ -0,0 +1,13 @@
+package com.ir.smartcity.location;
+
+public class OverviewPolyline {
+    private String points;
+    public String getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(String points) {
+
+    }
+}
